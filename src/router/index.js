e86230d864a411b9f978f36b3f0efcb6fc5e69b6
zhuang <zzz1678@126.com>
2022-03-03 22:25:14 +0800

    静态路由和动态路由临时合并

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import approvalsRouter from './modules/approvals'
+import departmentsRouter from './modules/departments'
+import employeesRouter from './modules/employees'
+import permissionRouter from './modules/permission'
+import attendancesRouter from './modules/attendances'
+import salarysRouter from './modules/salarys'
+import settingRouter from './modules/setting'
+import socialRouter from './modules/social'
+
 Vue.use(Router)
 
 /* Layout */
@@ -61,10 +70,21 @@ export const constantRoutes = [{
     { path: '*', redirect: '/404', hidden: true }
 ]
 
+export const asyncRouter = [
+    approvalsRouter,
+    departmentsRouter,
+    employeesRouter,
+    permissionRouter,
+    attendancesRouter,
+    salarysRouter,
+    settingRouter,
+    socialRouter
+]
+
 const createRouter = () => new Router({
     // mode: 'history', // require service support
     scrollBehavior: () => ({ y: 0 }),
-    routes: constantRoutes
+    routes: [...constantRoutes, ...asyncRouter]
 })
 
 const router = createRouter()
